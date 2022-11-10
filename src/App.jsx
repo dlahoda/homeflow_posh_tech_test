1d046636d7127d160d4bd39b3053278cefb12274
Denys Lahoda <denys.lahoda@gmail.com>
2022-11-10 14:15:16 +0200

    Add property filter by short_description logic
    Added jotai for simple state management.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState, useEffect } from 'react';
-import Header from './Header';
-import PropertyCard from './PropertyCard';
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import PropertyCard from "./PropertyCard";
+import { useAtomValue } from "jotai";
+import { searchTextAtom } from "./atoms";
 
 function App() {
+  const searchText = useAtomValue(searchTextAtom);
   const [properties, setProperties] = useState();
 
   // use this state to keep track of the user's saved/bookmarked properties
-  const [savedProperties, setSavedProperties] = useState([]);
+  // const [savedProperties, setSavedProperties] = useState([]);
 
   useEffect(() => {
     const fetchPropertyData = async () => {
-      const response = await fetch('/property-data.json');
+      const response = await fetch("/property-data.json");
       const json = await response.json();
 
       setProperties(json.result.properties.elements);
@@ -19,12 +22,21 @@ function App() {
     fetchPropertyData();
   }, []);
 
+  const propertyFilter = (property) =>
+    !searchText ||
+    new RegExp(searchText, "i").test(property["short_description"]);
+
   return (
     <div className="container mx-auto my-5">
       <Header />
 
       <div className="grid grid-cols-1 gap-4 mt-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {!!properties && properties.map((property) => <PropertyCard key={property.property_id} property={property} />)}
+        {!!properties &&
+          properties
+            .filter(propertyFilter)
+            .map((property) => (
+              <PropertyCard key={property.property_id} property={property} />
+            ))}
       </div>
     </div>
   );
--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,13 +1,27 @@
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch } from "react-icons/fa";
+import { useSetAtom } from "jotai";
+import { searchTextAtom } from "./atoms";
 
 function SearchBox() {
+  const setSearchText = useSetAtom(searchTextAtom);
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  };
+
   return (
     <div className="mt-5 relative">
-      <input placeholder="Enter a search term" className="px-5 py-3 border-gray-400 border rounded w-full" />
+      <input
+        onChange={handleChange}
+        placeholder="Enter a search term"
+        className="px-5 py-3 border-gray-400 border rounded w-full"
+      />
 
-      <FaSearch className="absolute top-3.5 right-3.5 text-gray-400" size={20} />
+      <FaSearch
+        className="absolute top-3.5 right-3.5 text-gray-400"
+        size={20}
+      />
     </div>
   );
-};
+}
 
 export default SearchBox;
